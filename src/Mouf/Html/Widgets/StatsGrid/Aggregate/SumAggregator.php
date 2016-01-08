ad12f29f6eedcec7e6c6579277e85bd1e06c2497
Anatolij Neciporenko <anatolij.neciporenko@gmail.com>
2016-01-08 22:56:06 +0200

    More

--- src/Mouf/Html/Widgets/StatsGrid/Aggregate/SumAggregator.php
+++ src/Mouf/Html/Widgets/StatsGrid/Aggregate/SumAggregator.php
@@ -12,54 +12,55 @@ use Mouf\Html\Widgets\StatsGrid\StatsValueDescriptor;
 
 /**
  * The most simple aggregator: it performs sums of the values and displays the total.
- * 
+ *
  * @author David Négrier
  * @Component
  */
 class SumAggregator implements AggregatorDescriptorInterface {
-	
-	private $statsColumnDescriptor;
-	private $statsValueDescriptor;
-	private $title;
-	
-	public function __construct(StatsColumnDescriptor $statsColumnDescriptor, StatsValueDescriptor $statsValueDescriptor, $title) {
-		$this->statsColumnDescriptor = $statsColumnDescriptor;
-		$this->statsValueDescriptor = $statsValueDescriptor;
-		$this->title = $title;
-	}
-	
-	/**
-	 * (non-PHPdoc)
-	 * @see AggregatorDescriptorInterface::getTitle()
-	 */
-	public function getTitle() {
-		return $this->title;
-	}
-	
-	/**
-	 * (non-PHPdoc)
-	 * @see AggregatorDescriptorInterface::getValueDescriptor()
-	 */
-	public function getValueDescriptor() {
-		return $this->statsValueDescriptor;
-	}
 
-	/**
-	 * (non-PHPdoc)
-	 * @see AggregatorDescriptorInterface::getStatsColumnDescriptor()
-	 */
-	public function getStatsColumnDescriptor() {
-		return $this->statsColumnDescriptor;
-	}
-	
-	/**
-	 * (non-PHPdoc)
-	 * @see AggregatorDescriptorInterface::getAggregatedValue()
-	 */
-	public function getAggregatedValue(array $values) {
-		$self = $this;
-		return array_sum(array_map(function($value) use($self) {
-			return $self->getValueDescriptor()->getValue($value);	
-		}, $values));
-	}
+    private $statsColumnDescriptor;
+    private $statsValueDescriptor;
+    private $title;
+
+    public function __construct(StatsColumnDescriptor $statsColumnDescriptor, StatsValueDescriptor $statsValueDescriptor, $title) {
+        $this->statsColumnDescriptor = $statsColumnDescriptor;
+        $this->statsValueDescriptor = $statsValueDescriptor;
+        $this->title = $title;
+    }
+
+    /**
+     * (non-PHPdoc)
+     * @see AggregatorDescriptorInterface::getTitle()
+     */
+    public function getTitle() {
+        return $this->title;
+    }
+
+    /**
+     * (non-PHPdoc)
+     * @see AggregatorDescriptorInterface::getValueDescriptor()
+     */
+    public function getValueDescriptor() {
+        return $this->statsValueDescriptor;
+    }
+
+    /**
+     * (non-PHPdoc)
+     * @see AggregatorDescriptorInterface::getStatsColumnDescriptor()
+     */
+    public function getStatsColumnDescriptor() {
+        return $this->statsColumnDescriptor;
+    }
+
+    /**
+     * (non-PHPdoc)
+     * @see AggregatorDescriptorInterface::getAggregatedValue()
+     */
+    public function getAggregatedValue(array $values) {
+        $self = $this;
+        if (count($values) == 0) return;
+        return array_sum(array_map(function($value) use($self) {
+            return $self->getValueDescriptor()->getValue($value);
+        }, $values));
+    }
 }
